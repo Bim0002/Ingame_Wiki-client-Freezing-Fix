69692fc9675647b23559b3d8c76e68ae9bd7e588
Dylan2 <75484286+Dylan002@users.noreply.github.com>
2023-02-23 18:08:38 +0100

    Update PythonApplication.cpp

--- PythonApplication.cpp
+++ PythonApplication.cpp
@@ -1,10 +1,10 @@
-search :
+//search :
 
 	if (CPythonWikiRenderTarget::instance().CanRenderWikiModules()) {
 		CWikiRenderTargetManager::Instance().ReleaseRenderTargetTextures();
 	}
 
-change it with this:
+//change it with this:
 
 CWikiRenderTargetManager::Instance().ReleaseRenderTargetTextures();
 
